492e662e217b4e39084d493b95eaa03c3fc8f5cf
M. Nabil Mu'afa <109450293+nabilmuafa@users.noreply.github.com>
2022-09-26 13:18:52 +0700

    Add kuis04.py

---
+++ quiz-H/kuis04.py
@@ -0,0 +1,13 @@
+biner = input("String biner\t:")
+
+pangkat = 1
+twos_comp = 0
+for i in range(len(biner)-1, -1, -1):
+    if i==0:
+        twos_comp += int(biner[i])*pangkat*-1
+        break
+    twos_comp += int(biner[i])*pangkat
+    pangkat *= 2
+    
+print("Signed integer\t:", twos_comp)
+    
