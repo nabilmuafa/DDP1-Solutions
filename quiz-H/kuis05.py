1678738fe465782a16d29dbfc26b3a7c011f8f0e
M. Nabil Mu'afa <109450293+nabilmuafa@users.noreply.github.com>
2022-10-07 14:36:40 +0700

    Add files via upload

---
+++ quiz-H/kuis05.py
@@ -0,0 +1,21 @@
+filename = input("nama file: ")
+
+try:
+    file = open(filename, "r")
+# menghandle jika baris tidak ditemukan
+except FileNotFoundError:
+    print("file tidak ditemukan!")
+    exit()
+
+# membaca isi file per baris ke variabel isi_file
+isi_file = file.readlines()
+
+# iterasi setiap baris
+for baris in isi_file:
+    # men-strip baris dan memisahnya menjadi per kata
+    per_kata = baris.strip().split()
+    
+    # iterasi tiap kata dalam urutan terbalik
+    for word in per_kata[::-1]:
+        print(word, end=" ")
+    print()
